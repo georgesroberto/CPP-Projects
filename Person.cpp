1310d6427a7fb6bcb1a8f0d7320d75f298ab152e
Roberto Georges <georgesroberto21@gmail.com>
2023-05-23 22:50:07 +0300

    Creating a class and calling it
    A beginner friendlyprogram to create a first class person and run it in main.

---
+++ Person.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Person
+{
+private:
+    string name;
+    int age;
+public:
+    Person(string name)
+    {
+        name=n;
+    }
+void DisplayName
+    {
+    cout<<"You can call me: "<<name<<endl;
+    }
+};
+int main()
+{
+    Person me("Georges Roberto");
+    me.DisplayName();
+    return 0;
+}
