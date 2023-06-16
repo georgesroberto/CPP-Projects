11a183e53c72ffdb54f887a2e9087119e16f2505
Roberto Georges <georgesroberto21@gmail.com>
2023-06-16 08:03:08 +0300

    Data Structures

---
+++ deskcheck.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int arr[4]={6,4,3,1};
+	int j=0;
+	int k=10;
+	for(j=0;j<4;j++)
+	{
+		if (arr[j]<k)
+		{
+			k+=arr[j];
+			cout<<"Take "<<k<<" as value of k"<<endl;
+		}
+		return 0;
+	}
+}
