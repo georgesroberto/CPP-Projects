1ccdef64006aff5d8f127ec6336cc034ef615d10
Roberto Georges <georgesroberto21@gmail.com>
2023-06-16 08:00:05 +0300

    Palindrome checker in cpp

---
+++ Palindrome.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int num,rev,rem,og;
+	cout<<"Input number: ";
+	cin>>og;
+    num=og;
+	rev=0;
+	while (num>0)
+	{
+		rem=num%10;//I extract last digit
+		rev=rev*10+rem;//store last dgtv@rev then option of adding other last digits
+		num=num/10;//to remove the extracted digit
+	}
+	cout<<"Reverse of "<<og<<" is: "<<rev<<endl;
+	
+	if(og==rev)
+	{
+		cout<<og<<" Is a palindrome"<<endl;
+	}
+	else
+	{
+		cout<<og<<" Is not a palindrome"<<endl;
+	}
+	
+	return 0;	
+}
